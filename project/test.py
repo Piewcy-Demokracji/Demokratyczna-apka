69bb71a3bdd19e791bcafaa9d5d1410c2c3de0fe
Dawidks294 <dawidks294@gmail.com>
2026-03-13 02:52:19 +0100

    Fix docstring formatting in test_function
    Corrected the formatting of the docstring and 'Args' section.

--- project/test.py
+++ project/test.py
@@ -1,9 +1,11 @@
 def test_function(a,b):
     """
     Ta funkcja jedynie testuje działanie generatora dokumentacji.
-    args:
+    
+    Args:
         a: jakaś zmienna
         b: inna zmienna
+        
     Returns:
         zwraca pierwszy argument
     """
