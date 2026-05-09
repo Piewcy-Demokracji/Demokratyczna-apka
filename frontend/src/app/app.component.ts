621e6afd11720571f84107a0cbc154d19a47cc7e
arturn0 <s223480@sggw.edu.pl>
2026-05-10 00:50:23 +0200

    yassification

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'voting-app';
+export class AppComponent implements OnInit {
+  appTitle = 'Voting App';
+  isLoggedIn = false;
+  isAdmin = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.isAdmin = this.authService.isAdmin();
+
+    if (this.isLoggedIn) {
+      this.authService.whoAmI().subscribe({
+        next: () => {
+          this.isAdmin = this.authService.isAdmin();
+          this.isLoggedIn = true;
+        },
+        error: () => {
+          this.isAdmin = false;
+          this.isLoggedIn = false;
+        }
+      });
+    }
+
+    this.authService.currentUser$.subscribe((user) => {
+      this.isLoggedIn = !!user || this.authService.isLoggedIn();
+      this.isAdmin = this.authService.isAdmin();
+    });
+  }
+
+  get showAdminLink(): boolean {
+    return this.isLoggedIn && this.isAdmin;
+  }
 }
